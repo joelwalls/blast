361e254b0f5d4322ef4239ef1b283485973aa0ca
Joel Paredes <admin@joelwalls.com>
2020-01-31 14:12:17 -0500

    Nuevas correcciones

--- app/Http/Controllers/BlastController.php
+++ app/Http/Controllers/BlastController.php
@@ -33,16 +33,16 @@ class BlastController extends Controller
         fwrite($db, $request->db);
         fclose($db);
 
-        $res = exec('makeblastdb -in files/db.fsa -title "Database" -dbtype nuc');
+        exec('makeblastdb -in files/db.fsa -title "Database" -dbtype nucl');
         exec('blastn -query files/sequence.fa -db files/db.fsa -out files/results.txt');
         $output = file_get_contents('files/results.txt');
 
-        // $files = glob('files/*');
-        // foreach ($files as $file){
-        //     if (is_file($file)) {
-        //         unlink($file);
-        //     }
-        // }
+        $files = glob('files/*');
+        foreach ($files as $file){
+            if (is_file($file)) {
+                unlink($file);
+            }
+        }
         return view('blast.index', [
             'results' => $output,
         ]);
